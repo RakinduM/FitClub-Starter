26ddf82eb34a41370c75d2e0775e2a778baa7f0b
RakinduM <rakindumarambe17@gmail.com>
2024-01-21 20:53:26 +0530

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Hero/Hero.css 	modified:   src/components/Hero/Hero.jsx 	new file:   src/components/Plans/Plans.css 	new file:   src/components/Plans/Plans.jsx 	new file:   src/components/Programs/Programs.css 	new file:   src/components/Programs/Programs.jsx 	new file:   src/components/Reasons/Reasons.css 	new file:   src/components/Reasons/Reasons.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import heart from '../../assets/heart.png'
+import calories from '../../assets/calories.png'
 
 const Hero = () => {
   return (
@@ -26,8 +30,51 @@ const Hero = () => {
                     <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
                 </div>
             </div>
+
+            {/*figures*/}
+            <div className="figures">
+                <div>
+                    <span>+140</span>
+                    <span>Expert Coaches</span>
+                </div>
+                <div>
+                    <span>+978</span>
+                    <span>Memebers Joined</span>
+                </div>
+                <div>
+                    <span>+40</span>
+                    <span>Fitness Programs</span>
+                </div>
+            </div>
+
+            {/*hero buttons*/}
+            <div className="hero-buttons">
+                <button className="btn">Get Started</button>
+                <button className="btn">Learn More</button>
+            </div>
+
         </div>
-        <div className="right-h">right</div>
+        <div className="right-h">
+            <button className='btn'>Join Now</button>
+            <div className="heart-rate">
+                <img src={heart} alt="" />
+                <span>Heart Rate</span>
+                <span>116 bpm</span>
+            </div>
+
+            {/*hero images*/}
+            <img src={hero_image} alt="" className='hero-image' />
+            <img src={hero_image_back} alt="" className='hero-image-back' />
+            {/*calories image*/}
+            <div className="calories">
+                <img src={calories} alt="" />
+                <div>
+                    <span>Calories Burned</span>
+                    <span>220 kcal</span>
+                </div>
+                
+            </div>
+        </div>
     </div>
   )
 }
